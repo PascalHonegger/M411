bb86b760eb5f4c505e76811c564fa1b8140af772
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2016-06-28 15:22:03 +0200

    Implementation fertiggestellt

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,19 +1,24 @@
 package controller;
 
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+
+import java.util.Stack;
 
 public class MainController {
 
 	@FXML
-	private Text attempts;
+	public Button continueButton;
 
 	@FXML
-	private GridPane chessboard;
+	private Text attempts;
 
 	@FXML
 	private ImageView queen1;
@@ -47,10 +52,102 @@ public class MainController {
 
 	private StringProperty attemptsProperty = new SimpleStringProperty();
 
+	private Stack<QueenController> remainingQueens = new Stack<>();
+	private Stack<QueenController> placedQueens = new Stack<>();
+
 	public void initialize() {
+		initializeQueens();
 
 		attempts.textProperty().bind(attemptsProperty);
+	}
 
-		setAmountOfAttempts(1);
+	@FXML
+	private void handleButtonAction(ActionEvent event) {
+		//Background thread so the GUI isn't blocked
+
+		new Thread(this::ContinueOrStartToPlaceQueens).start();
+	}
+
+	private void ContinueOrStartToPlaceQueens() {
+		continueButton.setDisable(true);
+
+		if (remainingQueens.empty()) {
+			QueenController latestQueen = placedQueens.pop();
+			latestQueen.setColumn(latestQueen.getColumn() + 1);
+			remainingQueens.push(latestQueen);
+		}
+
+		while (!remainingQueens.empty()) {
+			TryPlaceNextQueen();
+		}
+
+		continueButton.setDisable(false);
+	}
+
+	private void countAttempt()
+	{
+		amountOfAttempts++;
+		setAmountOfAttempts(amountOfAttempts);
+	}
+
+	private void TryPlaceNextQueen() {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		QueenController currentQueen = remainingQueens.pop();
+
+		if(placedQueens.empty())
+		{
+			countAttempt();
+
+			placedQueens.push(currentQueen);
+			return;
+		}
+
+		//Try place queen
+		while(currentQueen.getColumn() < 8)	{
+			countAttempt();
+
+			//Queen on same column
+			boolean queenInSameColumn = placedQueens.stream().map(q -> q.getColumn()).anyMatch(c -> currentQueen.getColumn() == c);
+			boolean queenInSameLeftDiagonal = placedQueens.stream().map(q -> q.getColumn() - q.getRow()).anyMatch(c -> currentQueen.getColumn() - currentQueen.getRow() == c);
+			boolean queenInSameRightDiagonal = placedQueens.stream().map(q -> q.getColumn() + q.getRow()).anyMatch(c -> currentQueen.getColumn() + currentQueen.getRow() == c);
+
+			if(queenInSameColumn || queenInSameLeftDiagonal || queenInSameRightDiagonal) {
+				currentQueen.setColumn(currentQueen.getColumn() + 1);
+			}
+			else {
+				//Can place queen
+				placedQueens.push(currentQueen);
+				return;
+			}
+		}
+
+		//Couldn't place queen!
+
+		if(currentQueen.getRow() == 8){
+			initialize();
+			return;
+		}
+
+		currentQueen.setColumn(0);
+		remainingQueens.push(currentQueen);
+		QueenController oldQueen = placedQueens.pop();
+		oldQueen.setColumn(oldQueen.getColumn() + 1);
+		remainingQueens.push(oldQueen);
+	}
+
+	private void initializeQueens() {
+		remainingQueens.push(new QueenController(queen8));
+		remainingQueens.push(new QueenController(queen7));
+		remainingQueens.push(new QueenController(queen6));
+		remainingQueens.push(new QueenController(queen5));
+		remainingQueens.push(new QueenController(queen4));
+		remainingQueens.push(new QueenController(queen3));
+		remainingQueens.push(new QueenController(queen2));
+		remainingQueens.push(new QueenController(queen1));
 	}
 }
---
+++ src/controller/QueenController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public class QueenController {
+
+    private ImageView queen;
+
+    public QueenController(ImageView queen)
+    {
+        this.queen = queen;
+    }
+
+    public int getColumn()
+    {
+        return GridPane.getColumnIndex(queen);
+    }
+
+    public void setColumn(int column)
+    {
+        GridPane.setColumnIndex(queen, column);
+    }
+
+    public int getRow()
+    {
+        return GridPane.getRowIndex(queen);
+    }
+
+    public void setRow(int row)
+    {
+        GridPane.setRowIndex(queen, row);
+    }
+}
