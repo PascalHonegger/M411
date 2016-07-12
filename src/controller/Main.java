66dbdaebc69870e1f10373a33f8b84642c0701ef
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2016-07-12 09:31:37 +0200

    Fenstergrösse besser angepasst

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
@@ -20,9 +21,13 @@ public class Main extends Application {
 		GridPane rootLayout = loader.load();
 
 		Scene scene = new Scene(rootLayout);
+
+		primaryStage.initStyle(StageStyle.UNIFIED);
 		primaryStage.getIcons().add(new Image("http://www.iconsfind.com/wp-content/uploads/2015/11/20151106_563c235abcd0c.png"));
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
+		primaryStage.setWidth(360 + 6);
+		primaryStage.setHeight(410 + 28);
 		primaryStage.setTitle("Das Damenproblem");
 		primaryStage.show();
 	}
